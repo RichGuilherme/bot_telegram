63c6b98943f874f126f0cc8740f4e1f449d1528b
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-05-15 14:41:26 -0300

    feat: setup do server

---
+++ serve/eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "indent": [
+        "error",
+        4
+      ],
+      "no-duplicate-imports": ["error", { "includeExports": true }],
+      "linebreak-style": [
+        "error",
+        "unix"
+      ],
+      "quotes": [
+        "error",
+        "double"
+      ],
+      "semi": [
+        "error",
+        "always"
+      ]
+    }
+  }
+];
