1923f38e2f516791b43b0b2f2277f0c3172db967
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-06-22 02:05:50 -0300

    chore: ajuste no PORT do listen

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,9 @@
 import 'dotenv/config'
 import createApp from './app';
+const PORT = process.env.PORT || 3000
 
 const app = createApp()
 
-app.listen({
-    hostname: "0.0.0.0",
-    port: process.env.PORT ? Number(process.env.PORT) : 3000
-}, () => {
-    console.log("Servidor rodando com sucesso!")
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
